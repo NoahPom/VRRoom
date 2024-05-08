378643a9909e90863b57ad48767817ee6e76b436
Sparen22 <153327834+Sparen22@users.noreply.github.com>
2024-05-08 13:28:53 -0700

    Added Water Assets

---
+++ Assets/Scripts/WaterRepeat.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterRepeat : MonoBehaviour
+{
+    public float waterSpeed;
+    public GameObject water;
+
+
+    void Update()
+    {
+        //water.transform.Translate
+    }
+}
